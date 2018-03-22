8a6efbe2753727a1f642e84c9b326aaa0e759555
Gedoba <tnawrocki1@gmail.com>
2018-03-22 12:44:47 +0100

    fixes

--- Lab6/testPL.c
+++ Lab6/testPL.c
@@ -27,33 +27,33 @@ void sethandler( void (*f)(int, siginfo_t*, void*), int sigNo) {
 }
 
 void mq_handler(int sig, siginfo_t *info, void *p) {
-	mqd_t *pin;
+	mqd_t *queues;
 	uint8_t ni;
 	unsigned msg_prio;
 
-	pin = (mqd_t *)info->si_value.sival_ptr;
+	queues = (mqd_t *)info->si_value.sival_ptr;
 
 	static struct sigevent not;
 	not.sigev_notify=SIGEV_SIGNAL;
 	not.sigev_signo=SIGRTMIN;
-	not.sigev_value.sival_ptr=pin;
+	not.sigev_value.sival_ptr=queues;
 
-	if(mq_notify(*pin, &not)<0)
+	if(mq_notify(*queues, &not)<0)
 		ERR("mq_notify");
 
 	for(;;){
-		if(mq_receive(*pin,(char*)&ni,1,&msg_prio)<1) {
+		if(mq_receive(*queues,(char*)&ni,1,&msg_prio)<1) {
 			if(errno==EAGAIN)
                 break;
 			else
                 ERR("mq_receive");
 		}
 		if(0==msg_prio)
-            printf("MQ: got timeout from %d.\n",ni);
-		else
-            printf("MQ:%d is a bingo number!\n",ni);
+            {
+                if(mq_receive(*queues,(char*)&ni,1,&msg_prio)<1)
+                    printf("Queue No %d received: %d\n",*queues-1,ni);
+            }
 	}
-
 }
 
 
@@ -77,14 +77,28 @@ void makeQueues(int n, char *queuesNames, mqd_t *queues){
 
     for(int i=0; i<n;i++){
         mq_unlink(queuesNames);
-        if((queues[i]=TEMP_FAILURE_RETRY(mq_open(queuesNames, O_RDWR|O_NONBLOCK | O_CREAT, 0600, &attr)))==(mqd_t)-1)
+        if((queues[i]=TEMP_FAILURE_RETRY(mq_open(queuesNames, O_RDWR |O_NONBLOCK |  O_CREAT, 0600, &attr)))==(mqd_t)-1)
             ERR("mq_open");
         else
             printf("Queue name: %s, dzielnik: %d, mkd_t: %d\n",queuesNames, queuesNames[1]-'a'+2, queues[i]); // dzielnik: queues[i]-1
         queuesNames[1] = 'a' + i + 1;
-        
+     
+	    static struct sigevent not;
+	    not.sigev_notify=SIGEV_SIGNAL;
+	    not.sigev_signo=SIGRTMIN;
+	    not.sigev_value.sival_ptr=&queues[i];
+	    if(mq_notify(queues[i], &not)<0)
+            ERR("mq_notify");   
     }
 }
+void receiveMessages(int queueNo, mqd_t *queues){
+        uint8_t rec;
+
+        if(TEMP_FAILURE_RETRY(mq_receive(queues[queueNo],(char*)&rec,1,NULL))<1)
+             ERR("mq_receive");
+         printf("Queue %d No received: %d\n",queues[queueNo]-1, rec);
+}
+
 void sendMessages(int n, char *queuesNames, mqd_t *queues)
 {
     srand(getpid());
@@ -92,14 +106,11 @@ void sendMessages(int n, char *queuesNames, mqd_t *queues)
     for(int i=0; i<5; i++){
         uint8_t raNo = (uint8_t)rand() % 100;
         int queueNo = rand() % n;
-        uint8_t rec;
         if(TEMP_FAILURE_RETRY(mq_send(queues[queueNo],(const char*)&raNo,1,0)))
             ERR("mq_send");
         printf("Queue %d No sent: %d\n",queues[queueNo]-1, raNo);
-            
-        // if(TEMP_FAILURE_RETRY(mq_receive(queues[queueNo],(char*)&rec,1,NULL))<1)
-        //     ERR("mq_receive");
-        // printf("Queue %d No received: %d\n",queues[queueNo]-1, rec);
+
+        //receiveMessages(queueNo, queues);
     }
 }
 
@@ -112,9 +123,13 @@ int main(int argc, char** argv) {
         usage(); 
     mqd_t *queues = (mqd_t*)malloc(n*sizeof(mqd_t));
     char *queuesNames = (char*)malloc(4*sizeof(char));
+
+    
+	sethandler(mq_handler,SIGRTMIN);
     //sleep(2);
     makeQueues(n, queuesNames, queues);
     sendMessages(n, queuesNames, queues);
+
 
     for(int i=0; i<n; i++)
     {
