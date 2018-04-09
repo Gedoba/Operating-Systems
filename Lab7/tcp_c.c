9e4c9db75d9792f26af823e377df4fffae453ba0
Gedoba <tnawrocki1@gmail.com>
2018-04-09 11:02:34 +0200

    added udp

--- Lab7/tcp_c.c
+++ Lab7/tcp_c.c
@@ -1,4 +1,3 @@
-//serial
 #define _GNU_SOURCE 
 #include <stdio.h>
 #include <stdlib.h>
@@ -88,44 +87,34 @@ ssize_t bulk_write(int fd, char *buf, size_t count){
 	}while(count>0);
 	return len ;
 }
-void prepare_request(char **argv,int32_t data[1]){
-    int32_t rNo;
-    rNo = (int32_t)rand() % 1000 + 1;
-    printf("my no %d\n", rNo);
-	data[0]=(int32_t)htonl(rNo);
+void prepare_request(char **argv,int32_t data[5]){
+	data[0]=htonl(atoi(argv[3]));
+	data[1]=htonl(atoi(argv[4]));
+	data[2]=htonl(0);
+	data[3]=htonl((int32_t)(argv[5][0]));
+	data[4]=htonl(1);
 }
-void print_answer(int32_t data[1]){
-    
-
+void print_answer(int32_t data[5]){
+	if(ntohl(data[4]))
+		printf("%d %c %d = %d\n", ntohl(data[0]),(char)ntohl(data[3]), ntohl(data[1]), ntohl(data[2]));
+	else printf("Operation impossible\n");
 }
 void usage(char * name){
-	fprintf(stderr,"USAGE: %s jebac \n",name);
+	fprintf(stderr,"USAGE: %s domain port  operand1 operand2 operation \n",name);
 }
 int main(int argc, char** argv) {
-    srand(getpid());    
 	int fd;
-	int32_t data[1];
-	if(argc!=3) {
+	int32_t data[5];
+	if(argc!=6) {
 		usage(argv[0]);
 		return EXIT_FAILURE;
 	}
 	if(sethandler(SIG_IGN,SIGPIPE)) ERR("Seting SIGPIPE:");
 	fd=connect_socket(argv[1],argv[2]);
-    int hits = 0;
-    while(hits<3){
-	    prepare_request(argv,data);
-        int32_t sent = ntohl(data[0]);
-	    if(bulk_write(fd,(char *)data,sizeof(int32_t[1]))<0) ERR("write:");
-        if(bulk_read(fd,(char *)data,sizeof(int32_t[1]))<0) ERR("read:");
-        if(sent == ntohl(data[0]))
-            printf("HIT: %d\n", ntohl(data[0]));
-        
-        usleep(750000);
-        hits++;
-    }
-
-    
-
+	prepare_request(argv,data);
+	if(bulk_write(fd,(char *)data,sizeof(int32_t[5]))<0) ERR("write:");
+	if(bulk_read(fd,(char *)data,sizeof(int32_t[5]))<(int)sizeof(int32_t[5])) ERR("read:");
+	print_answer(data);
 	if(TEMP_FAILURE_RETRY(close(fd))<0)ERR("close");
 	return EXIT_SUCCESS;
 }
--- Lab7/tcp_s.c
+++ Lab7/tcp_s.c
@@ -1,4 +1,3 @@
-//serial
 #define _GNU_SOURCE 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,7 +18,6 @@
 
 #define BACKLOG 3
 volatile sig_atomic_t do_work=1 ;
-int32_t max = 0;
 void sigint_handler(int sig) {
 	do_work=0;
 }
@@ -98,43 +96,51 @@ ssize_t bulk_write(int fd, char *buf, size_t count){
 	}while(count>0);
 	return len ;
 }
-void calculate(int32_t data[1]){
-    int32_t no = ntohl(data[0]);
-    printf("received %d\n", no);
-    if(max<no)
-        max = no;
-    printf("%d\n", max);
+void calculate(int32_t data[5]){
+	int32_t op1,op2,result,status=1;
+	op1=ntohl(data[0]);
+	op2=ntohl(data[1]);
+	switch((char)ntohl(data[3])){
+		case '+':result=op1+op2;
+			 break;
+		case '-':result=op1-op2;
+			 break;
+		case '*':result=op1*op2;
+			 break;
+		case '/':if(!op2) status=0 ;
+				 else result=op1/op2;
+				 break;
+		default: status=0;
+	}
+	data[4]=htonl(status);
+	data[2]=htonl(result);
 }
 void communicate(int cfd){
 	ssize_t size;
-	int32_t data[1];
-    int hits = 0;
-    while(hits<3){
-	    if((size=bulk_read(cfd,(char *)data,sizeof(int32_t[1])))<0) ERR("read:");
-	    if(size==(int)sizeof(int32_t[1])){
-            calculate(data);
-        data[0] = htonl(max);
-	    if(bulk_write(cfd,(char *)data,sizeof(int32_t[1]))<0) ERR("write:");
-		hits++;
-	    }
-
-    }
-
+	int32_t data[5];
+	if((size=bulk_read(cfd,(char *)data,sizeof(int32_t[5])))<0) ERR("read:");
+	if(size==(int)sizeof(int32_t[5])){
+		calculate(data);
+		if(bulk_write(cfd,(char *)data,sizeof(int32_t[5]))<0&&errno!=EPIPE) ERR("write:");
+	}
 	if(TEMP_FAILURE_RETRY(close(cfd))<0)ERR("close");
 }
-void doServer(int fdT){
+void doServer(int fdL, int fdT){
 	int cfd,fdmax;
 	fd_set base_rfds, rfds ;
 	sigset_t mask, oldmask;
 	FD_ZERO(&base_rfds);
+	FD_SET(fdL, &base_rfds);
 	FD_SET(fdT, &base_rfds);
+	fdmax=(fdT>fdL?fdT:fdL);
 	sigemptyset (&mask);
 	sigaddset (&mask, SIGINT);
 	sigprocmask (SIG_BLOCK, &mask, &oldmask);
 	while(do_work){
 		rfds=base_rfds;
-		if(pselect(fdT+1,&rfds,NULL,NULL,NULL,&oldmask)>0){
-			cfd=add_new_client(fdT);
+		if(pselect(fdmax+1,&rfds,NULL,NULL,NULL,&oldmask)>0){
+			if(FD_ISSET(fdL,&rfds)) cfd=add_new_client(fdL);
+			else  cfd=add_new_client(fdT);
 			if(cfd>=0)communicate(cfd);
 		}else{
 			if(EINTR==errno) continue;
@@ -144,20 +150,24 @@ void doServer(int fdT){
 	sigprocmask (SIG_UNBLOCK, &mask, NULL);
 }
 int main(int argc, char** argv) {
-	int fdT;
+	int fdL,fdT;
 	int new_flags;
-	if(argc!=2) {
+	if(argc!=3) {
 		usage(argv[0]);
 		return EXIT_FAILURE;
 	}
 	if(sethandler(SIG_IGN,SIGPIPE)) ERR("Seting SIGPIPE:");
 	if(sethandler(sigint_handler,SIGINT)) ERR("Seting SIGINT:");
-	fdT=bind_tcp_socket(atoi(argv[1]));
+	fdL=bind_local_socket(argv[1]);
+	new_flags = fcntl(fdL, F_GETFL) | O_NONBLOCK;
+	fcntl(fdL, F_SETFL, new_flags);
+	fdT=bind_tcp_socket(atoi(argv[2]));
 	new_flags = fcntl(fdT, F_GETFL) | O_NONBLOCK;
 	fcntl(fdT, F_SETFL, new_flags);
-	doServer(fdT);
+	doServer(fdL,fdT);
+	if(TEMP_FAILURE_RETRY(close(fdL))<0)ERR("close");
+	if(unlink(argv[1])<0)ERR("unlink");
 	if(TEMP_FAILURE_RETRY(close(fdT))<0)ERR("close");
 	fprintf(stderr,"Server has terminated.\n");
 	return EXIT_SUCCESS;
-    
 }
